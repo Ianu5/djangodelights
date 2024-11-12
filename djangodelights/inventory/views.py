cbe1430a732e68dc47d5a9274cef651a34406ce0
Jesaja Hur <j.hur@iqul.de>
2024-11-12 12:21:17 +0100

    added templates for base home and ingredient list

--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,3 +1,42 @@
+from typing import Any
 from django.shortcuts import render
+from .models import Ingredient, MenuItem, Purchase, RecipeRequirement
+from django.views.generic import TemplateView, ListView, CreateView
 
 # Create your views here.
+class IngredientView(ListView):
+    model = Ingredient
+    template_name = "ingredient_list.html"
+    context_object_name = "ingredients"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        
+        cost = sum(ingredient.price_per_unit for ingredient in context['ingredients'])
+        context['cost'] = cost
+        return context
+
+class MenuView(ListView):
+    model = MenuItem
+    template_name = "menu.html"
+    context_object_name = "menu"
+
+
+class PurchaseView(ListView):
+    model = Purchase
+    template_name = "purchases.html"
+    context_object_name = "purchases"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        revenue = sum(purchase.menu_item.price for purchase in context['purchase'])
+        context['revenue'] = revenue
+        return context
+
+
+class ProfitView():
+    ...
+
+
+def home(request):
+    return render(request, 'inventory/home.html')
